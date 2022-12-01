8443cfc2ca7cadcbe11b3a5bb6cb7dad5a6de795
Aidos <ajdos0735@gmaul.com>
2022-12-01 23:47:35 +0600

    Сделал 34 задачу

---
+++ zadacha-34/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] arr = GetArray(4,100,1000);
+Console.WriteLine($"[{String.Join(", ", arr)}]");
+
+Console.WriteLine(Chet(arr));
+
+int[] GetArray(int size, int minV, int maxV){
+    int[] mas = new int[size];
+    for(int i=0;i<size;i++){
+        mas[i] = new Random().Next(minV,maxV);
+    }
+    return mas;
+}
+
+int Chet(int[] array){
+    int count = 0;
+    for(int i=0;i<array.Length;i++){
+        if (array[i] % 2 == 0){
+            count += 1;
+        }
+    }
+    return count;
+}
