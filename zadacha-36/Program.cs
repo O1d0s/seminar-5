40792a81545629a9f5ff5cbd701009b3da50edd1
Aidos <ajdos0735@gmaul.com>
2022-12-02 00:12:36 +0600

    Сделал задачу 36

---
+++ zadacha-36/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] arr = GetArray(4,-10,50);
+Console.WriteLine($"[{String.Join(", ", arr)}]");
+
+Console.WriteLine(NeChet(arr));
+
+int[] GetArray(int size, int minV, int maxV){
+    int[] mas = new int[size];
+    for(int i=0;i<size;i++){
+        mas[i] = new Random().Next(minV,maxV);
+    }
+    return mas;
+}
+
+int NeChet(int[] array){
+    int sum = 0;
+    for(int i=0;i<array.Length;i++){
+        if (i % 2 == 1){
+            sum += array[i];
+        }
+    }
+    return sum;
+}
